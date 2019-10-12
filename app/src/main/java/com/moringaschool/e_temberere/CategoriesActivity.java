50792c5888f0899974876caa31c0e9c21aecf50a
ahiodette <ahiode6@gmail.com>
2019-10-12 21:43:52 +0200

    ListView implementation

--- app/src/main/java/com/moringaschool/e_temberere/CategoriesActivity.java
+++ app/src/main/java/com/moringaschool/e_temberere/CategoriesActivity.java
@@ -14,26 +14,25 @@ import butterknife.ButterKnife;
 
 
 public class CategoriesActivity extends AppCompatActivity {
-   
-    public static final String TAG = CategoriesActivity.class.getSimpleName();
 
-    private String[] categories = new String[]{"Parks", "Museums", "Lakes", "Others places(Hotels, Cultural shops,...)"};
+    ListView list;
+
+     String[] details = {"Parks", "Museums", "Lakes", "Caves","Genocide memorials","Others places(Hotels, Cultural shops,...)"};
+     int pictures[]={R.drawable.park,R.drawable.muse,R.drawable.kivu,R.drawable.caves,R.drawable.genocide,R.drawable.round};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        list = (ListView) findViewById(R.id.categoryList);
+        CategoriesAdapter customAdapter = new CustomerAdapter()
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String title = intent.getStringExtra("title");
 //        CategoriesAdapter adapter = new CategoriesAdapter(this, categories);
 //        cList.setAdapter(adapter);
-        cList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-            }
-        });
     }
 }
--- app/src/main/java/com/moringaschool/e_temberere/CategoriesAdapter.java
+++ app/src/main/java/com/moringaschool/e_temberere/CategoriesAdapter.java
@@ -1,38 +1,46 @@
-//package com.moringaschool.e_temberere;
-//
-//import android.content.Context;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.BaseAdapter;
-//
-//public class CategoriesAdapter extends BaseAdapter {
-//    private Context print;
-//    private String[] categs;
-//
-//    public CategoriesAdapter(Context print, String[] categs) {
-//        super();
-//        this.print = print;
-//        this.categs = categs;
-//    }
-//
-//    @Override
-//    public int getCount() {
-//        return categs.length;
-//    }
-//
-//    @Override
-//    public Object getItem(int position) {
-//        String category = categs[position];
-//        return String.format("%s",category);
-//    }
-//
-//    @Override
-//    public long getItemId(int position) {
-//        return 0;
-//    }
-//
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        return null;
-//    }
-//}
+package com.moringaschool.e_temberere;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class CategoriesAdapter extends BaseAdapter {
+    Context print;
+    String[] details;
+    int pictures[];
+    LayoutInflater inflater;
+
+    public CategoriesAdapter(Context print, String[] details, int[] pictures) {
+        this.print = print;
+        this.details = details;
+        this.pictures = pictures;
+
+    }
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        view = inflater.inflate(R.layout.activity_categories,null);
+
+        return null;
+    }
+
+
+}
