211a5e4c6320fef094d679ce07b139bf4351f199
ahiodette <ahiode6@gmail.com>
2019-10-19 16:44:23 +0200

    Create Unchangeable class

---
+++ app/src/main/java/com/moringaschool/e_temberere/Unchangeable.java
@@ -0,0 +1,6 @@
+package com.moringaschool.e_temberere;
+
+public class Unchangeable {
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/";
+    public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
+}
