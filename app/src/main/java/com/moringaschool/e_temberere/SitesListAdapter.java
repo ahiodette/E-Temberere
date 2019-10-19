67abd1c72c0e1ab28a54381b29b1420605c62ce8
ahiodette <ahiode6@gmail.com>
2019-10-19 21:16:42 +0200

    Implement ViewHolder

--- app/src/main/java/com/moringaschool/e_temberere/SitesListAdapter.java
+++ app/src/main/java/com/moringaschool/e_temberere/SitesListAdapter.java
@@ -1,4 +1,30 @@
 package com.moringaschool.e_temberere;
 
-public class SitesListAdapter {
+import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.moringaschool.e_temberere.models.Business;
+import com.moringaschool.e_temberere.ui.CategoriesAdapter;
+
+import java.util.List;
+
+import butterknife.BindView;
+
+public class SitesListAdapter extends RecyclerView.Adapter<CategoriesAdapter.SitesViewHolder> {
+    private List<Business> sList;
+    private Context myContext;
+
+    public SitesListAdapter(List<Business> sList, Context myContext) {
+        this.sList = sList;
+        this.myContext = myContext;
+    }
+
+    public class SitesViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.sitePicture) ImageView picture;
+        @BindView(R.id.siteName) TextView name;
+        @
+    }
 }
