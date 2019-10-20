fda87802d7b20401f94f63346ad281d0f1014daf
ahiodette <ahiode6@gmail.com>
2019-10-20 18:10:07 +0200

    Override FragmentPagerAdapter methods

--- app/src/main/java/com/moringaschool/e_temberere/adapters/SitePagerAdapter.java
+++ app/src/main/java/com/moringaschool/e_temberere/adapters/SitePagerAdapter.java
@@ -2,21 +2,38 @@ package com.moringaschool.e_temberere.adapters;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.moringaschool.e_temberere.models.Business;
+import com.moringaschool.e_temberere.ui.SiteDetailFragment;
+
+import java.util.List;
 
 public class SitePagerAdapter extends FragmentPagerAdapter {
 
-    private 
+    private List<Business> sites ;
+
+    public SitePagerAdapter(@NonNull FragmentManager fm, int behavior, List<Business> sites) {
+        super(fm, behavior);
+        this.sites = sites;
+    }
+
+
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        return null;
+        return SiteDetailFragment.newInstance(sites.get(position));
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return sites.size();
     }
 
+    @Override
+    public CharSequence getPageTitle(int position){
+        return sites.get(position).getName();
+    }
 
 }
