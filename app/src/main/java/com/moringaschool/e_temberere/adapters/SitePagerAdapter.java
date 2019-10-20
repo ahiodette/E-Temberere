d641b1f0236c5876e1327930b9abae33e3cdb038
ahiodette <ahiode6@gmail.com>
2019-10-20 17:58:04 +0200

    Create SitePagerAdapter

---
+++ app/src/main/java/com/moringaschool/e_temberere/adapters/SitePagerAdapter.java
@@ -0,0 +1,22 @@
+package com.moringaschool.e_temberere.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class SitePagerAdapter extends FragmentPagerAdapter {
+
+    private 
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+
+}
