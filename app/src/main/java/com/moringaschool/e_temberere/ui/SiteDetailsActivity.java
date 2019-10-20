01e098cb34b9a8ec1b930a0307ab2c9457294cdf
ahiodette <ahiode6@gmail.com>
2019-10-20 15:59:01 +0200

    Add site details fragment

--- app/src/main/java/com/moringaschool/e_temberere/ui/SiteDetailsActivity.java
+++ app/src/main/java/com/moringaschool/e_temberere/ui/SiteDetailsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.moringaschool.e_temberere.R;
+
 public class SiteDetailsActivity extends AppCompatActivity {
 
     @Override
