3e202823c0088892361012af46432f6792921c1e
ahiodette <ahiode6@gmail.com>
2019-10-26 21:29:34 +0200

    Validate the registration form

--- app/src/main/java/com/moringaschool/e_temberere/ui/SignupActivity.java
+++ app/src/main/java/com/moringaschool/e_temberere/ui/SignupActivity.java
@@ -1,16 +1,24 @@
 package com.moringaschool.e_temberere.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.moringaschool.e_temberere.R;
 
 import butterknife.BindView;
@@ -19,7 +27,7 @@ import butterknife.ButterKnife;
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
     @BindView(R.id.createUserButton) Button newUserButton;
-    @BindView(R.id.nameEditText) Button newUserName;
+    @BindView(R.id.nameEditText) EditText newUserName;
     @BindView(R.id.emailEditText) EditText newUserEmail;
     @BindView(R.id.passwordEditText) EditText newUserPassword;
     @BindView(R.id.confirmPasswordEditText) EditText passConfirmation;
@@ -67,5 +75,82 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         final String email = newUserEmail.getText().toString();
         String password = newUserPassword.getText().toString();
         String confirm = passConfirmation.getText().toString().trim();
+        name = newUserName.getText().toString().trim();
+
+        boolean validEmail = isValidEmail(email);
+        boolean validName = isValidName(userName);
+        boolean validPassword = isValidPassword(password, confirm);
+        if (!validEmail || !validName || !validPassword) return;
+        progressDialog.show();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+
+                if (task.isSuccessful()){
+                    createFirebaseUserProfile(task.getResult().getUser());
+                }
+                else {
+                    Toast.makeText(SignupActivity.this, "Authentication failed.", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+    private void createAuthStateListener(){
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+    }
+    private boolean isValidEmail(String email){
+        boolean isGoodEmail = (email != null && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail){
+            newUserEmail.setError("Please enter a valid address");
+            return false;
+        }
+        return isGoodEmail;
+    }
+
+    private boolean isValidName(String name){
+        if (name.equals("")){
+            newUserName.setError("Please enter your name");
+            return false;
+        }
+        return true;
+    }
+     private boolean isValidPassword(String password, String confirmPassword){
+        if (password.length()<6){
+            newUserPassword.setError("Please create a password containing at least 6 characters");
+            return false;
+        }
+        else if (!password.equals(confirmPassword)){
+            newUserPassword.setError("Passwords do not match");
+            return false;
+        }
+        return true;
+     }
+
+     private void createFirebaseUserProfile(final FirebaseUser user){
+         UserProfileChangeRequest addProfileName = new UserProfileChangeRequest.Builder()
+            .setDisplayName(name).build();
+         user.updateProfile(addProfileName).addOnCompleteListener(new OnCompleteListener<Void>() {
+             @Override
+             public void onComplete(@NonNull Task<Void> task) {
+                 if (task.isSuccessful()){
+                     Toast.makeText(SignupActivity.this, "The displayed name has been set", Toast.LENGTH_LONG).show();
+                 }
+             }
+         });
+
     }
 }
+
