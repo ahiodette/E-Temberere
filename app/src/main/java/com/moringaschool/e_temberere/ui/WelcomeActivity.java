11fec4917e7ff6826b6718429036e7e48252f549
ahiodette <ahiode6@gmail.com>
2019-11-03 16:11:56 +0200

    Create shake animation

--- app/src/main/java/com/moringaschool/e_temberere/ui/WelcomeActivity.java
+++ app/src/main/java/com/moringaschool/e_temberere/ui/WelcomeActivity.java
@@ -2,15 +2,36 @@ package com.moringaschool.e_temberere.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 import com.moringaschool.e_temberere.R;
 
-public class WelcomeActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.welcomeButton) Button welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
+        welcome.setOnClickListener(this);
+        Animation shake = AnimationUtils.loadAnimation(this,R.anim.shake);
+        getStarted.startAnimation(shake);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==welcome){
+            Intent intent = new Intent(WelcomeActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
